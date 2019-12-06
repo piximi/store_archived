39c36c618b161402da56d1831db5e2d1441c7383
Allen Goodman <allen.goodman@icloud.com>
2019-12-06 10:44:28 -0500

    removed index.d.ts

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import { StateType, ActionType } from 'typesafe-actions';
-
-export type RootAction = ActionType<typeof import('src/actions').default>;
-
-declare module 'typesafe-actions' {
-  interface Types {
-    RootAction: RootAction;
-  }
-}

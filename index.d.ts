de0a6df4baeb18815421c6ea3a1e46b679e8afda
Allen Goodman <allen.goodman@icloud.com>
2019-12-06 10:43:41 -0500

    fix tests

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
-import * as actions from './src/actions/classifier';
 import { StateType, ActionType } from 'typesafe-actions';
 
-export type RootAction = ActionType<typeof actions>;
+export type RootAction = ActionType<typeof import('src/actions').default>;
 
 declare module 'typesafe-actions' {
   interface Types {
